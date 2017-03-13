75bcec7f341cb39e064845a163f599519fff89ee
Lucas David <ld492@drexel.edu>
2017-03-13 16:40:23 -0300

    Add Magic tests

---
+++ test/core/cards/MagicTest.java
@@ -0,0 +1,47 @@
+package core.cards;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ldavid
+ */
+public class MagicTest {
+
+    public MagicTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testDefaultLackeys() {
+        assertNotNull(Magic.DEFAULT_CARDS);
+        assertFalse(Magic.DEFAULT_CARDS.isEmpty());
+    }
+
+    @Test
+    public void testToString() {
+        System.out.println(Magic.DEFAULT_CARDS.stream()
+                .findFirst()
+                .orElse(null));
+    }
+}
