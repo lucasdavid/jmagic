8b3320283c7c6f94b3e3ac62db8a283f8ef1dfa2
Lucas David <ld492@drexel.edu>
2017-03-28 22:44:40 -0300

    add abstract modifier in SingleHarmful class

--- src/hearthstone/core/cards/SingleHarmful.java
+++ src/hearthstone/core/cards/SingleHarmful.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 
-public class SingleHarmful extends Harmful {
+public abstract class SingleHarmful extends Harmful {
     public SingleHarmful(String name, int damage, int manaCost) {
         super(name, damage, manaCost);
     }
