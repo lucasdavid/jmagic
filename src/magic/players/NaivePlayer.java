c64cd86a7897c4bdce559b68f6b2c621004d6976
Lucas David <ld492@drexel.edu>
2017-05-17 14:17:20 -0300

    update NaivePlayer

--- src/magic/players/NaivePlayer.java
+++ src/magic/players/NaivePlayer.java
@@ -1,14 +1,24 @@
 package magic.players;
 
 import magic.core.actions.Action;
+import magic.core.actions.DeclareBlockersAction;
 import magic.core.actions.PlayAction;
 import magic.core.actions.validation.rules.players.active.HasLandsToPlayIt;
+import magic.core.cards.creatures.Creature;
 import magic.core.cards.lands.Land;
 import magic.core.experts.IExpert;
 import magic.core.states.State;
 import magic.core.states.TurnSteps;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class NaivePlayer extends BasicPlayer {
 
@@ -35,7 +45,29 @@ public class NaivePlayer extends BasicPlayer {
         }
 
         if (state.step == TurnSteps.DECLARE_BLOCKERS) {
-            // Declare blockers...
+            List<Creature> attackers = state
+                .turnsPlayerState()
+                    .attackers.entrySet().stream()
+                    .filter(e -> this.equals(e.getValue()))
+                    .map(Map.Entry::getKey)
+                    .sorted(Comparator.comparingInt(Creature::effectiveDamage))
+                    .collect(Collectors.toCollection(ArrayList::new));
+
+            if (!attackers.isEmpty()) {
+                // There are creature attacking me, declare blockers...
+                Map<Creature, Creature> blockers = new HashMap<>();
+
+                myState.field.cards().stream()
+                    .filter(c -> c instanceof Creature)
+                    .map(c -> (Creature) c)
+                    .sorted(Comparator.comparingInt(Creature::effectiveLife))
+                    .forEachOrdered(blocker -> {
+                        Creature attacker = attackers.remove(0);
+                        blockers.put(blocker, attacker);
+                    });
+
+                return new DeclareBlockersAction(blockers);
+            }
         }
 
         // Don't know what to do. Ask for the superclass.
